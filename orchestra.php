01b18dc03dea85e30f4fe9aa3102a7656e819952
crynobone <crynobone@gmail.com>
2012-11-25 10:34:52 +0800

    Include some docblock
    Signed-off-by: crynobone <crynobone@gmail.com>

--- orchestra.php
+++ orchestra.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Register Robotix as Orchestra Resources
+|--------------------------------------------------------------------------
+|
+| Wait for `orchestra.started` to add this event.
+|
+*/
+
 Event::listen('orchestra.started', function ()
 {
 	$robots = Orchestra\Resources::make('robotix', array(
--- routes.php
+++ routes.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Robotix Route
+|--------------------------------------------------------------------------
+|
+| Robots.txt should be located at the main `robots.txt` url.
+|
+*/
+
 Route::any('robots.txt', function ()
 {
 	// Make sure profiler is off.
